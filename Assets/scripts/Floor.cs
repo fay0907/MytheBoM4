df2a7f78e6467527d82d647b01621de9ada8a0b0
SvenVanDerMeij <38007@ma-web.nl>
2024-05-23 11:37:52 +0200

    ja, ik push niet zo vaak, sorry aidan
    letterlijk alles van de afgelopen 3 weken

---
+++ Assets/scripts/Floor.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    private healthManager help;
+    private Basic boterhamworst;
+    float attackspeed = 50f;
+    public int state = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boterhamworst = FindObjectOfType<Basic>();
+        help = FindObjectOfType<healthManager>();
+    }
+    void SetTransformX(float n)
+    {
+        transform.position = new Vector3(transform.position.x, n, transform.position.z);
+    }
+    float timePassed = 0f;
+    void Update()
+    {
+        //Debug.Log("attackspeed is " + attackspeed);
+        //Debug.Log("attackspeed is " + attackspeed);
+       // Debug.Log("timepassed is " + timePassed);
+        
+            
+        
+        timePassed += Time.deltaTime;
+        //Debug.Log(timePassed);
+        if (timePassed > attackspeed)
+        {
+            //Debug.Log("piss");
+            //Debug.Log("holy shit hij werkt");
+            timePassed = 0f;
+            boterhamworst.Kill();
+        }
+
+
+
+        
+            
+            
+        
+       // Debug.Log("mijn state is " + state + " en timePassed is " +  timePassed);
+    }
+
+    // Update is called once per frame
+
+
+    public void Change()
+    {
+        if (state == 0)
+        {
+            SetTransformX(2.5f);
+            help.test2();
+            // transform.position = Lavafloor.transform.position;
+            state = 1;
+            attackspeed = 0.5f;
+        } else if (state == 1)
+        {
+           
+            state = 2;
+        }
+        else if (state == 2)
+        {
+            
+            state = 3;
+        } else 
+        {
+            
+        }
+        
+    }
+
+
+   
+            
+
+}
