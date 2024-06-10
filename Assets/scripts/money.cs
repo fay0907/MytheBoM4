781c4179031d2516b074c1b9239a531576b8eaf6
Aidan <38162@ma-web.nl>
2024-06-10 12:14:27 +0200

    git drama fix

---
+++ Assets/scripts/money.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+    internal int moneyvalue = 0;
+}
---
+++ Assets/scripts/towerattack.cs
@@ -0,0 +1,112 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerAttack : MonoBehaviour
+{
+    private List<enemy> enemiesInRange = new List<enemy>();
+    private enemy currentEnemy;
+    private Money money;
+    private bool isAttacking;
+    public float atkspd = 1; // attack speed
+    internal int damage = 2;
+    internal int hpbeforeattack;
+
+    void Start()
+    {
+        // Initialize Money instance
+        money = new Money();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("enemy"))
+        {
+            enemy Enemy = other.GetComponent<enemy>();
+            if (Enemy != null && !enemiesInRange.Contains(Enemy))
+            {
+                enemiesInRange.Add(Enemy);
+
+                if (!isAttacking)
+                {
+                    StartAttacking();
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Tower"))
+        {
+            enemy Enemy = other.GetComponent<enemy>();
+            if (Enemy != null && enemiesInRange.Contains(Enemy))
+            {
+                enemiesInRange.Remove(Enemy);
+
+                if (currentEnemy == Enemy)
+                {
+                    currentEnemy = null;
+                    isAttacking = false;
+                    CancelInvoke("Attack");
+                    if (enemiesInRange.Count > 0)
+                    {
+                        StartAttacking();
+                    }
+                }
+            }
+        }
+    }
+
+    void StartAttacking()
+    {
+        if (enemiesInRange.Count > 0)
+        {
+            currentEnemy = enemiesInRange[0];
+            hpbeforeattack = currentEnemy.hp; // Initialize hpbeforeattack when starting to attack
+            isAttacking = true;
+            InvokeRepeating("Attack", atkspd, atkspd); // Repeats the method at specified attack speed
+        }
+    }
+
+    void Attack()
+    {
+        if (currentEnemy == null)
+        {
+            StopAttacking();
+            return;
+        }
+
+        Debug.Log("Attacking: " + currentEnemy.name);
+
+        int damageDealt = damage; // Define the damage to be dealt
+
+        currentEnemy.hp -= damageDealt; // Deal damage
+        money.moneyvalue += (hpbeforeattack - currentEnemy.hp); // Update moneyvalue based on HP difference
+
+        hpbeforeattack = currentEnemy.hp; // Update hpbeforeattack after attack
+
+        bool enemyIsDead = currentEnemy.isDead(); // Check if the enemy is dead
+
+        if (enemyIsDead)
+        {
+            enemiesInRange.Remove(currentEnemy);
+            currentEnemy = null;
+
+            if (enemiesInRange.Count > 0)
+            {
+                currentEnemy = enemiesInRange[0];
+                hpbeforeattack = currentEnemy.hp; // Initialize hpbeforeattack for the new enemy
+            }
+            else
+            {
+                StopAttacking();
+            }
+        }
+    }
+
+    void StopAttacking()
+    {
+        isAttacking = false;
+        CancelInvoke("Attack");
+    }
+}
